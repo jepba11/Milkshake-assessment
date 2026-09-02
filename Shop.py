3ee0b2dcad7d7238433b502965ea8d4ba249cac4
jepba11 <jepba11.gaming@gmail.com>
2026-09-03 11:21:51 +1200

    Jeans purchase

---
+++ Shop.py
@@ -0,0 +1,14 @@
+AmountOfCustomers = input("How many customers are in the line?: ")
+#print(f"The number of customers in the line is: {AmountOfCustomers}")
+Productlist=["Jeans", "T-Shirts", "Shoes", "Socks", "Skirts"]
+print(f"Items for sale are: {Productlist}")
+print("Jeans are $55 would you like to buy any?")
+buyJeans = input("Y or N:")
+if buyJeans == "Y":
+    numofJeans = int(input("How many Jeans would you like to buy?: "))
+    return()
+else:
+    print("No Jeans will be purchased.")
+    return()
+if input("Is that everything? Y or N:") == "Y"S
+    print("your total is...")
