de21851c976c7b30a4841b2bc882081087a2c43a
jepba11 <jepba11.gaming@gmail.com>
2026-09-08 11:30:43 +1200

    restarted

--- Shop.py
+++ Shop.py
@@ -1,22 +1,18 @@
+#define constants
+DISCOUNT = 0.75
+SPDISCOUNT = 0.9
+
+#define functions
+
+#welcome msg
 print("Hello and welcome to the cash register program.")
+customerAtRegister = True
+#main loop
+while customerAtRegister == True:
+ 
+ isCustomerAtRegister = input("Is there another customer in the line? Y or N: ")
 
-AmountOfCustomers = int(input("How many customers are in the line?: "))
-
-while AmountOfCustomers > 3:
-    print("Line is missmanaged. Please divert extra customers to other registers.")
-    AmountOfCustomers = int(input("How many customers are now in the line?:"))
-print("Line is now manageable.")
-
-#print(f"The number of customers in the line is: {AmountOfCustomers}")
-Productlist=["Jeans", "T-Shirts", "Shoes", "Socks", "Skirts"]
-print(f"Items for sale are: {Productlist}")
-print("Jeans are $55 would you like to buy any?")
-buyJeans = input("Y or N:")
-if buyJeans == "Y":
-    numofJeans = int(input("How many Jeans would you like to buy?: "))
-else:
-    print("No Jeans will be purchased.")
-if input("Is that everything? Y or N:") == "Y":
-    totalPrice = (numofJeans * 55)
-    print(f"your total is...{totalPrice}")
-#else:
+ if isCustomerAtRegister == "Y":
+     customerAtRegister = True
+ elif isCustomerAtRegister == "N":
+     customerAtRegister = False
