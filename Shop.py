8623ea1f9821f05b3a51a26e254b97e685c45004
jepba11 <jepba11.gaming@gmail.com>
2026-09-09 11:40:14 +1200

    working on new customer function

--- Shop.py
+++ Shop.py
@@ -3,20 +3,65 @@ DISCOUNT = 0.75 #default sale discount
 SPDISCOUNT = 0.9 #special discount for 3+ items or purchases of over $100
 
 #define lists
-ItemsForSale = ["Jeans","T-Shirts","Shoes","Socks","Skirts"]
-SalePrice = []
+ItemsForSale = ['Jeans','T-Shirts','Shoes','Socks','Skirts'] #list name says it all
+
+RetailPrice = [55, 30, 45, 5, 25] #retail price for items
+#print([f"${price:.2f}" for price in RetailPrice]) #test list logic
+
+PriceAfterSale = [DISCOUNT * price for price in RetailPrice]
+#print([f"${price:.2f}" for price in PriceAfterSale]) #test list logic
+
+TotalCustomerSales = [] #stores total sales for the session
 
 #define functions
+def newCustomer():
+
+    #per-customer lists
+    Order = []
+    RetailCost = []
+    SaleCost = []
+    SpecialDiscount = []
+
+    #print welcome message and sale info
+    print("Welcome to this Factory Outlet! We currently have a sale of 25% off all stocked items!\n"
+          "If you purchase 3 or more items, or if your total purchase is over $100,\n"
+          "you will receive an additional 10% off your total purchase.") #small intro msg split across multiple lines for readability
+
+    #print stocked items
+    itemSelected = False
+    while itemSelected == False:
+     print("\nAvailable items:")
+     for i in range(len(ItemsForSale)):
+         print(f"- {ItemsForSale[i]} -Item code: {i+1} - ${RetailPrice[i]:.2f} - Sale Price: ${PriceAfterSale[i]:.2f}") #lists all stocked items and their retail + discount price
+    
+     print("\n What would you like to buy today?")
+     itemTypes = input("Please enter the item code for the item(s) the customer would like to buy, sperated by a comma.\n"
+           "E.g. 1,2,3 for Jeans, T-Shirts, and Shoes: ").split(",") #splits input into a list of item codes, seperated by a comma
+     print(itemTypes) #test list logic
+     itemTypes = [item.strip() for item in itemTypes] #removes whitespace from each item in the list
+     print(itemTypes) #test list logic
+     if len(itemTypes) == 0:
+         print("No items were selected. Please try again.")
+         itemSelected = False
+     else:
+         itemSelected = True
+
 
 #welcome msg
 print("Hello and welcome to the cash register program.")
 customerAtRegister = True
+
 #main loop
 while customerAtRegister == True:
- 
- isCustomerAtRegister = input("Is there another customer in the line? Y or N: ")
 
- if isCustomerAtRegister == "Y":
-     customerAtRegister = True
- elif isCustomerAtRegister == "N":
-     customerAtRegister = False
+    newCustomer()
+
+
+    isCustomerAtRegister = input("Is there another customer in the line? Y or N: ").strip().upper()
+
+    if isCustomerAtRegister == "Y":
+        customerAtRegister = True
+    elif isCustomerAtRegister == "N":
+        customerAtRegister = False
+    else:
+        print("Please enter Y or N.")
